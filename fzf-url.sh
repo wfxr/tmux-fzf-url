ab15ae51e1aff97bf5090cbbbab5246ade8dd52b
Carlos Barroso <carlos.barroso@teracloud.io>
2020-08-24 00:08:49 -0300

    Improved regexp to remove false positives

--- fzf-url.sh
+++ fzf-url.sh
@@ -22,7 +22,7 @@ urls=($(echo "$content" |grep -oE '(https?|ftp|file):/?//[-A-Za-z0-9+&@#/%?=~_|!
 wwws=($(echo "$content" |grep -oE 'www\.[a-zA-Z](-?[a-zA-Z0-9])+\.[a-zA-Z]{2,}(/\S+)*'                  |sed 's/^\(.*\)$/http:\/\/\1/'))
 ips=($(echo  "$content" |grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}(:[0-9]{1,5})?(/\S+)*' |sed 's/^\(.*\)$/http:\/\/\1/'))
 gits=($(echo "$content" |grep -oE '(ssh://)?git@\S*' | sed 's/:/\//g' | sed 's/^\(ssh\/\/\/\)\{0,1\}git@\(.*\)$/https:\/\/\2/'))
-k8s=($(echo "$content" |grep -oE "^([A-Za-z0-9]+-)+[A-Za-z0-9]+"))
+k8s=($(echo "$content" |grep -oE "^([A-Za-z0-9]+-){2,}[A-Za-z0-9]+"))
 merge() {
     for item in "$@" ; do
         echo "$item"
